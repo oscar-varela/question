9574393a2e0868d049ae4929b4cf52daac863819
oscar <oscar_yasael@hotmail.com>
2019-09-27 10:29:47 -0500

    dysplaying choises

--- ChoiseQuestion.java
+++ ChoiseQuestion.java
@@ -1,21 +1,33 @@
+import java.util.LinkedList;
+
 public class ChoiseQuestion extends Question
 {
    private int choiseNumer;
+   private LinkedList<String> choises;
     public ChoiseQuestion()
     {
      choiseNumer = 0;
+     choises = new LinkedList<>();
     }
     
     public void addChoise(String choise , boolean correct)
-    {   choiseNumer++;
+    {   this.choises.add(choise);
+        choiseNumer++;
         if(correct)
-        {super.setAnswer(String.valueOf(choiseNumer));
+        {
+            super.setAnswer(String.valueOf(choiseNumer));
         }
     }
     
     public void display()
     {   
         super.display();
+        int counter = 1;
         System.out.println("Dysplaying choices... ");
+        for(String choise : this.choises)
+        {
+            System.out.println(counter++ + ")" +choise);
+            
+        }
     }
 }
